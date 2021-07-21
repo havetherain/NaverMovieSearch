9706e781b50248343d3bf8690ec3805097543b26
havetherain@gamil.com <havetherain@gmail.com>
2021-07-21 10:39:21 +0900

    feat: 네트워크 통신 기본 코드 구현

---
+++ NaverMovieSearch/Model/MovieItemVO.swift
@@ -0,0 +1,8 @@
+//
+//  MovieItemVO.swift
+//  NaverMovieSearch
+//
+//  Created by 김지우 on 2021/07/21.
+//
+
+import Foundation
--- NaverMovieSearch/Network/NetworkService.swift
+++ NaverMovieSearch/Network/NetworkService.swift
@@ -10,18 +10,22 @@ import Alamofire
 
 protocol NetworkService {
     typealias networkSuccessResult = (resCode: Int, resResult: Any)
+    func get<T: Codable>(_ URL: String, params: Parameters?, type: T.Type, completion: @escaping (NetworkResult<networkSuccessResult>) -> Void)
     func post<T: Codable>(_ URL: String, parameters: Parameters?, type: T.Type, completion: @escaping (NetworkResult<networkSuccessResult>) -> Void)
 }
 
 extension NetworkService {
-    func post<T: Codable>(_ URL: String, parameters: Parameters?, type: T.Type, completion: @escaping (NetworkResult<networkSuccessResult>) -> Void) {
+    func get<T: Codable>(_ URL: String, params: Parameters? = nil, type: T.Type, completion: @escaping (NetworkResult<networkSuccessResult>)->Void) {
         guard let encodeUrl = URL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-            print("Invalid URL")
+            print("GET Invalid URL")
             return
         }
-        AF.request(encodeUrl, method: .post, parameters: parameters, encoding: JSONEncoding.default).validate(statusCode: 200 ..< 300).responseData { res in
+
+        let headers : HTTPHeaders = ["X-Naver-Client-Id": clientId, "X-Naver-Client-Secret": clientSecret]
+
+        AF.request(encodeUrl, method: .get, parameters: params, headers: headers).validate(statusCode: 200 ..< 300).responseData { res in
             switch res.result {
-            case .success :
+            case .success:
                 if let value = res.value {
                     let decoder = JSONDecoder()
                     do {
@@ -29,10 +33,10 @@ extension NetworkService {
                         let data = try decoder.decode(T.self, from: value)
                         completion(.networkSuccess((resCode, data)))
                     } catch(let error) {
-                        print("\(encodeUrl) >> Decoding Error: \(error)")
+                        print("GET \(encodeUrl) >> Decoding Error: \(error)")
                     }
                 }
-            case .failure(let err) :
+            case .failure(let err):
                 if let error = err as NSError?, error.code == -1009 {
                     completion(.networkFail)
                 } else {
@@ -40,6 +44,67 @@ extension NetworkService {
                     completion(.networkError((resCode, err.localizedDescription)))
                 }
             }
+        }
+    }
+
+    func post<T: Codable>(_ URL: String, parameters: Parameters?, type: T.Type, completion: @escaping (NetworkResult<networkSuccessResult>) -> Void) {
+        guard let encodeUrl = URL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            print("POST Invalid URL")
+            return
+        }
+        AF.request(encodeUrl, method: .post, parameters: parameters, encoding: JSONEncoding.default).validate(statusCode: 200 ..< 300).responseData { res in
+            switch res.result {
+            case .success:
+                if let value = res.value {
+                    let decoder = JSONDecoder()
+                    do {
+                        let resCode = res.response?.statusCode ?? 0
+                        let data = try decoder.decode(T.self, from: value)
+                        completion(.networkSuccess((resCode, data)))
+                    } catch(let error) {
+                        print("POST \(encodeUrl) >> Decoding Error: \(error)")
+                    }
+                }
+            case .failure(let err):
+                if let error = err as NSError?, error.code == -1009 {
+                    completion(.networkFail)
+                } else {
+                    let resCode = res.response?.statusCode ?? 0
+                    completion(.networkError((resCode, err.localizedDescription)))
+                }
+            }
+        }
+    }
+}
+
+extension NetworkService {
+    private var clientId: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "APIKey", ofType: "plist") else {
+                fatalError("Couldn't find file 'APIKey.plist'")
+            }
+
+            let plist = NSDictionary(contentsOfFile: filePath)
+
+            guard let value = plist?.object(forKey: "NaverMoviewAPIClientID") as? String else {
+                fatalError("Couldn't find key 'NaverMoviewAPIClientID' in 'APIKey.plist'")
+            }
+            return value
+        }
+    }
+
+    private var clientSecret: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "APIKey", ofType: "plist") else {
+                fatalError("Couldn't find file 'APIKey.plist'")
+            }
+
+            let plist = NSDictionary(contentsOfFile: filePath)
+
+            guard let value = plist?.object(forKey: "NaverMoviewAPIClientSecret") as? String else {
+                fatalError("Couldn't find key 'NaverMoviewAPIClientSecret' in 'APIKey.plist'")
+            }
+            return value
         }
     }
 }
